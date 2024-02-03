265a003da706a5c6da002c9da4c7a478cc657f53
Fatih Victor Kilic <me@fklc.dev>
2024-02-02 22:52:25 -0500

    Created S3 bucket and upload URL creation endpoint

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const { PrismaClient } = require('@prisma/client')
+const { getSignedUrl } = require('@aws-sdk/s3-request-presigner')
+const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3')
 
 const prisma = new PrismaClient()
 const app = express()
@@ -18,6 +20,25 @@ app.post(`/listing`, async (req, res) => {
   }
 
   return res.status(201).json(result);
+});
+
+app.get(`/listing-create-upload-url`, async (req, res) => {
+  // Never store access key and secret key in code. I'll do it here for simplicity and we don't have a server to store it.
+  const client = new S3Client({
+    credentials: {
+      accessKeyId: "AKIAZK2CV2WX2N5LE7QH",
+      secretAccessKey: "mq3JJXCuuj9j5gbTqyi4RyGnVLua3tNiw0Vlz64c"
+    },
+    region: "us-east-1"
+  });
+
+  const key = crypto.randomUUID().replaceAll("-", "") + ".jpg";
+  const command = new PutObjectCommand({
+    Bucket: "csc301-uhome-images",
+    Key: key,
+  });
+
+  return res.json(await getSignedUrl(client, command, { expiresIn: 60 * 60 }));
 });
 
 const server = app.listen(3000, () =>
