05fa22800d467ddf962f06b8f540caa486551ac1
Fatih Victor Kilic <me@fklc.dev>
2024-02-02 21:51:55 -0500

    Implemented create listing endpoint

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,6 +6,20 @@ const app = express()
 
 app.use(express.json())
 
+app.post(`/listing`, async (req, res) => {
+  const result = await prisma.rentalListing.create({ data: req.body })
+    .catch(e => {
+      console.error(e);
+      return false;
+    });
+
+  if (!result) {
+    return res.status(400).json(`Unable to create listing.`);
+  }
+
+  return res.status(201).json(result);
+});
+
 const server = app.listen(3000, () =>
   console.log(`Server started at: http://localhost:3000`),
 )
