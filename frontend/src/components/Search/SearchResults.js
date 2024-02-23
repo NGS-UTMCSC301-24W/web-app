b2b890a4ab7a97f01863e5700338785e07ae8453
stevenhe-sww <stevenhe211@gmail.com>
2024-02-23 16:16:25 -0500

    display search result in card form

--- frontend/src/components/Search/SearchResults.js
+++ frontend/src/components/Search/SearchResults.js
@@ -1,15 +1,30 @@
 import React from 'react';
+import Filter from '../Filter/Filter';
 
 const SearchResults = ({ location }) => {
   const { results } = location.state || { results: [] };
+
   return (
     <div>
       <h2>Search Results:</h2>
-      <ul>
+
+      {/* Filter Component */}
+      <Filter />
+
+      {/* Display Results in Cards with Three Cards in Each Row */}
+      <div className="row row-cols-1 row-cols-md-3 g-4">
         {results.map((result) => (
-          <li key={result.id}>{result.title}</li>
+          <div className="col" key={result.id}>
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">{result.title}</h5>
+                <p className="card-text"><strong>Address:</strong> {result.address}</p>
+                <p className="card-text"><strong>Description:</strong> {result.description}</p>
+              </div>
+            </div>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 };
