6109b020898c70e3088ce8f05b1dc1c6fd72d46d
ShivaniAnands2320 <46700556+ShivaniAnand2320@users.noreply.github.com>
2024-02-25 14:36:27 -0500

    removed Home/js and Lisitngs.js is our landing page, so updated App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
-import Home from './components/Home';
 import Login from './components/Login';
 import Register from './components/RegisterForm';
 import Filter from "./components/Filter";
@@ -17,9 +16,6 @@ const App = () => {
       <div>
         <nav>
           <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
             <li>
               <Link to="/login">Login</Link>
             </li>
@@ -40,7 +36,6 @@ const App = () => {
 
         <hr />
         <Switch>
-          <Route path="/" exact component={Home} />
           <Route path="/login" component={Login} />
           <Route path="/register" component={Register} />
           <Route path="/filter" component={Filter} />
